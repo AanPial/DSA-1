8b48ede04bad0232c635e84b783d3bae6fb0fbcb
AanPial <165553604+AanPial@users.noreply.github.com>
2024-06-21 22:35:47 +0600

    Update SAL.c

--- DSA/SAL.c
+++ DSA/SAL.c
@@ -1,4 +1,58 @@
 #include <stdio.h>
+void print(int arr[], int i)
+{
+    printf("\nThe current array list is : ");
+    for (int j = 0; j < i; j++)
+    {
+        printf("%d ", arr[j]);
+    }
+}
+void delete(int *arr, int pos, int i)
+{
+    for (int j = pos; j < i; j++)
+    {
+        arr[j] = arr[j + 1];
+    }
+}
+void bubblesort(int *arr, int i)
+{
+    int temp;
+    for (int j = 0; j < i; j++)
+    {
+        for (int k = 0; k < i; k++)
+        {
+            if (arr[k] > arr[k + 1])
+            {
+                temp = arr[k];
+                arr[k] = arr[k + 1];
+                arr[k + 1] = temp;
+            }
+        }
+    }
+}
+int binarysrch(int *arr,int i,int sd)
+{
+    int l=0,r=i-1;
+    
+
+    while(l<=r)
+    {
+        int mid=(l+r)/2;
+        if(arr[mid]==sd)
+        {
+            return mid;
+        }
+        else if(arr[mid]<sd)
+        {
+            l=mid+1;
+        }
+        else
+        {
+            r=mid-1;
+        }
+    }
+    return -1;
+}
 int main()
 {
     int n, choice, pos, new_val, s_val, i = 0, flag;
@@ -10,61 +64,23 @@ int main()
     while (1)
     {
     err:
-        printf("\n\nMax Size of SAL is set to %d\n", n);
-        printf("\nMenu : ");
-        printf("\n1.Insert \n2.Update\n3.Search\n4.Print\n5.Delete (bonus)\n6.Print Current Array\n7.Delete occurence\n8.Bubble sort\n0.EXIT");
-        printf("\n\nEnter choice : ");
+        printf("\nMenu : \n");
+        printf("1.Print Entire array (index 0 to len)\n2.Access a specific index\n3.Update the value of a specific index\n4.Linear Search\n5.Insert new value at a specific indexn\n6.Delete from an index");
+        printf("\n7.Delete nth occurrence of a value\n8.Bubble Sort the existing array\n9.Binary Search on the array\n10.EXIT\n");
+        printf("\nEnter choice: ");
         scanf("%d", &choice);
 
-        if (choice == 0)
+        if (choice == 1)
         {
-            break;
-        }
-        else if (choice == 1)
-        {
-            printf("Enter element to insert: ");
-            scanf("%d", &new_val);
-            printf("\nEnter the position (index) to insert(current array length is: %d, so choose an index between 0 and %d): ", i, i);
-            scanf("%d", &pos);
-            if(pos<0 || pos>n || pos>i+1 || n == i)
-            {
-                 printf("Out of Bound");
-                 goto err;
-            }
-            else if (i < n)
-            {
-                printf("%d inserted at index %d\n", new_val, pos);
-                if (pos == i)
-                {
-                    arr[pos] = new_val;
-                }
-                else if (pos >= 0 && pos < i)
-                {
-                    int length = i + 1;
-                    for (int j = length; j >= pos; j--)
-                    {
-                        arr[j + 1] = arr[j];
-                    }
-                    arr[pos] = new_val;
-                }
-                else
-                {
-                    printf("Incorrect positional value. The value can not be inserted.");
-                    goto err;
-                }
-                i++;
-            }
-            else
-            {
-                printf("Sorry, the array List is full");
-            }
-            printf("\nThe current array list is:");
-            for (int j = 0; j < i; j++)
-            {
-                printf("%d ", arr[j]);
-            }
+            print(arr, i);
         }
         else if (choice == 2)
+        {
+            printf("\nEnter the index to print: ");
+            scanf("%d", &pos);
+            printf("\nThe val at index %d: %d", pos, arr[pos]);
+        }
+        else if (choice == 3)
         {
             printf("\nEnter the index to update: ");
             scanf("%d", &pos);
@@ -73,7 +89,7 @@ int main()
             arr[pos] = new_val;
             printf("The new val at index %d: %d", pos, arr[pos]);
         }
-        else if (choice == 3)
+        else if (choice == 4)
         {
             int j = 0;
             printf("\nEnter the Search Key : ");
@@ -96,73 +112,91 @@ int main()
                 printf("\nThe search key is not in the array\n");
             }
         }
-        else if (choice == 4)
-        {
-            printf("\nEnter the index to print: ");
-            scanf("%d", &pos);
-            printf("\nThe val at index %d: %d", pos, arr[pos]);
-        }
         else if (choice == 5)
+        {
+            printf("Enter element to insert: ");
+            scanf("%d", &new_val);
+            printf("\nEnter the position (index) to insert(current array length is: %d, so choose an index between 0 and %d): ", i, i);
+            scanf("%d", &pos);
+            if (pos < 0 || pos > n || pos > i + 1 || n == i)
+            {
+                printf("Incorrect positional value. The value can not be inserted.");
+                goto err;
+            }
+            else if (i < n)
+            {
+                if (pos == i)
+                {
+                    printf("%d inserted at index %d\n", new_val, pos);
+                    arr[pos] = new_val;
+                }
+                else if (pos >= 0 && pos < i)
+                {
+                    printf("%d inserted at index %d\n", new_val, pos);
+                    int length = i + 1;
+                    for (int j = length; j >= pos; j--)
+                    {
+                        arr[j + 1] = arr[j];
+                    }
+                    arr[pos] = new_val;
+                }
+                else
+                {
+                    printf("Incorrect positional value. The value can not be inserted.");
+                    goto err;
+                }
+                i++;
+            }
+            else
+            {
+                printf("Sorry, the array List is full");
+            }
+            print(arr, i);
+        }
+
+        else if (choice == 6)
         {
             printf("\nEnter the position (index) to delete from: ");
             scanf("%d", &pos);
             if (pos <= i)
             {
                 printf("\nDeleted from position %d\n", pos);
-                for (int j = pos; j < i; j++)
-                {
-                    arr[j] = arr[j + 1];
-                }
+                delete (arr, pos, i);
                 i--;
             }
             else
             {
                 printf("\nIncorrect pos value\n");
             }
-            printf("\nThe current array list is:");
-            for (int j = 0; j < i; j++)
-            {
-                printf("%d ", arr[j]);
-            }
+            print(arr, i);
         }
-        else if(choice==6)
-        {
-            printf("\nThe current array list is:");
-            for (int j = 0; j < i; j++)
-            {
-                printf("%d ", arr[j]);
-            }
-        }
-        else if(choice == 7)
+        else if (choice == 7)
         {
             printf("Enter value to delete : ");
-            scanf("%d",&new_val);
+            scanf("%d", &new_val);
             printf("Enter occurance to delete : ");
-            scanf("%d",&pos);
-            flag=0;
-            int x=0;
+            scanf("%d", &pos);
+            flag = 0;
+            int x = 0;
             int y;
             for (int j = 0; j < i; j++)
             {
 
-                if(new_val == arr[j] )
+                if (new_val == arr[j])
                 {
                     x++;
-                    if(x==pos){
-                       flag=1;
-                       y=j;
-                       break;
+                    if (x == pos)
+                    {
+                        flag = 1;
+                        y = j;
+                        break;
                     }
                 }
-
             }
-            if(flag==1)
+            if (flag == 1)
             {
                 printf("\nDeleted from position %d\n", pos);
-                for (int j = y; j < i; j++)
-                {
-                    arr[j] = arr[j + 1];
-                }
+                delete (arr, y, i);
                 i--;
             }
             else
@@ -170,35 +204,36 @@ int main()
                 printf("Occurance doesn't exist .\n");
                 goto err;
             }
-            printf("\nThe current array list is:");
-            for (int j = 0; j < i; j++)
-            {
-                printf("%d ", arr[j]);
-            }
+            print(arr, i);
         }
         else if (choice == 8)
         {
             printf("Sorted by bubble sort Algorithm.\n");
-            int temp;
-            for(int j=0;j<i;j++)
+            bubblesort(arr, i);
+            print(arr, i);
+        }
+        else if(choice == 9)
+        {
+            printf("\nEnter the Search Key for binary search: ");
+            scanf("%d", &s_val);
+            bubblesort(arr,i);
+            printf("\nSorted by bubble sort Algorithm.");
+            print(arr, i);
+            int x=binarysrch(arr,i,s_val);
+            if(x == -1)
             {
-                for(int k=0;k<i;k++)
-                {
-                    if(arr[k]>arr[k+1])
-                    {
-                        temp=arr[k];
-                        arr[k]=arr[k+1];
-                        arr[k+1]=temp;
-                    }
-                }
+                printf("\nThe search key is not in the array\n");
             }
-            printf("\nThe current array list is:");
-            for (int j = 0; j < i; j++)
+            else
             {
-                printf("%d ", arr[j]);
+                printf("\nThe search key %d is at index %d\n", s_val, x);
             }
         }
-        
+        else if (choice == 10)
+        {
+            printf("Exited!");
+            break;
+        }
         else
         {
             printf("\nWrong Choice. Enter again.\n\n");
